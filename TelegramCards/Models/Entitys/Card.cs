e520f24cb3d42c6e09d2f537353ef5439c31a714
IvanSobolev <i@bezlikiy.ru>
2025-04-01 09:52:20 +0300

    feat: Data context

--- TelegramCards/Models/Entitys/Card.cs
+++ TelegramCards/Models/Entitys/Card.cs
@@ -3,11 +3,11 @@
 public class Card
 {
     public long Id { get; set; }
-    public long? OwnerId { get; set; }
-    public long? BaseCardId { get; set; }
+    public long OwnerId { get; set; }
+    public long BaseCardId { get; set; }
     public DateTime GenerateCard { get; set; }
     public DateTime UserReceivedCard { get; set; }
     
-    public CardBase? BaseCard { get; set; }
-    public User? Owner { get; set; }
+    public CardBase BaseCard { get; set; }
+    public User Owner { get; set; }
 }
