e411c32011bcb3a7e53d156ff688cbb5dd0eaadf
IvanSobolev <i@bezlikiy.ru>
2025-04-01 10:12:03 +0300

    feat: card and user interface

---
+++ TelegramCards/Repositories/Interfaces/ICardRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace TelegramCards.Repositories.Interfaces;
+
+public interface ICardRepository
+{
+    Task GenerateNewCardToUserAsync(long userTelegramId);
+    Task GetUserCardsAsync(long ownerId);
+    Task SendCardsAsync(long cardId, long newOwnerId);
+}
---
+++ TelegramCards/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace TelegramCards.Repositories.Interfaces;
+
+public interface IUserRepository
+{
+    Task AddNewUserAsync(long telegramId, string username);
+    Task EditUsernameAsync(long telegramId, string newUsername);
+    Task GetUserByUsernameAsync(string username);
+}
