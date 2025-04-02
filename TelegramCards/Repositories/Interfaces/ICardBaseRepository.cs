3c4ae4d553d9e1e7f90d3c639b41419ab7a20ecd
IvanSobolev <i@bezlikiy.ru>
2025-04-02 14:00:50 +0300

    feat: add card interface repository

---
+++ TelegramCards/Repositories/Interfaces/ICardBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using TelegramCards.Models.Entitys;
+using TelegramCards.Models.Enum;
+
+namespace TelegramCards.Repositories.Interfaces;
+
+public interface ICardBaseRepository
+{
+    Task<CardBase> AddNewCardBaseAsync(long adminId, Rarity rarity, string photoUrl, int pointsNumber);
+    Task<ICollection<CardBase>> GetCardBasesAsync(long adminId, int page, int pageSize);
+    Task<CardBase> GetCardBasesAsync(long adminId, long cardId);
+    Task<CardBase> UpdateCardBaseAsync(long adminId, long cardId, Rarity? rarity = null, string? photoUrl = null, int? pointsNumber = null);
+    Task DeleteCardBaseAsync(long adminId, long id);
+}
