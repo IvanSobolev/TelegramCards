5f867be6a8022168321cd490c7649ae4a4857e22
IvanSobolev <i@bezlikiy.ru>
2025-04-02 16:33:28 +0300

    feat: user Entity framework repository (and turn off autoincrement telegram id)

--- TelegramCards/Models/Entitys/User.cs
+++ TelegramCards/Models/Entitys/User.cs
@@ -1,9 +1,11 @@
-﻿using TelegramCards.Models.Enum;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using TelegramCards.Models.Enum;
 
 namespace TelegramCards.Models.Entitys;
 
 public class User
 {
+    [DatabaseGenerated(DatabaseGeneratedOption.None)]
     public long TelegramId { get; set; }
     public string Username { get; set; }
     public Roles Role { get; set; }
--- TelegramCards/Repositories/Interfaces/IUserRepository.cs
+++ TelegramCards/Repositories/Interfaces/IUserRepository.cs
@@ -5,7 +5,7 @@ namespace TelegramCards.Repositories.Interfaces;
 public interface IUserRepository
 {
     Task<User> AddNewUserAsync(long telegramId, string username);
-    Task<User> GetUserByUsernameAsync(string username);
-    Task<User> GetUserByTelegramIdAsync(long telegramId);
-    Task<User> EditUsernameAsync(long telegramId, string newUsername);
+    Task<User?> GetUserByUsernameAsync(string username);
+    Task<User?> GetUserByTelegramIdAsync(long telegramId);
+    Task<User?> EditUsernameAsync(long telegramId, string newUsername);
 }
