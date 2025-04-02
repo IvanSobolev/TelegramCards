c0c3cbfd86e0917087bf0a9c92e649a9d130ee22
IvanSobolev <i@bezlikiy.ru>
2025-04-02 19:40:09 +0300

    fix: Data base for bussines logic

--- TelegramCards/Models/Entitys/Card.cs
+++ TelegramCards/Models/Entitys/Card.cs
@@ -1,12 +1,15 @@
-﻿namespace TelegramCards.Models.Entitys;
+﻿using TelegramCards.Models.Enum;
+
+namespace TelegramCards.Models.Entitys;
 
 public class Card
 {
     public long Id { get; set; }
     public long OwnerId { get; set; }
-    public long BaseCardId { get; set; }
-    public DateTime GenerateCard { get; set; }
-    public DateTime UserReceivedCard { get; set; }
+    public Rarity RarityLevel { get; set; }
+    public long CardIndex { get; set; }
+    public DateTime GenerationDate { get; set; }
+    public DateTime ReceivedCard { get; set; }
     
     public CardBase BaseCard { get; set; }
     public User Owner { get; set; }
--- TelegramCards/Models/Entitys/CardBase.cs
+++ TelegramCards/Models/Entitys/CardBase.cs
@@ -4,9 +4,10 @@ namespace TelegramCards.Models.Entitys;
 
 public class CardBase
 {
-    public long Id { get; set; }
-    public ICollection<Card> Cards { get; set; }
     public Rarity RarityLevel { get; set; }
+    public long CardIndex { get; set; }
     public string CardPhotoUrl { get; set; }
-    public int PointsNumber { get; set; }
+    public int Points { get; set; }
+    
+    public ICollection<Card> Cards { get; set; }
 }
