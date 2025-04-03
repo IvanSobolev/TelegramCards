552599e1df42a57ba7eba628e6618f91e91ee5da
IvanSobolev <i@bezlikiy.ru>
2025-04-03 09:45:45 +0300

    fix: interface card repository for new implementation and logic

--- TelegramCards/Repositories/Interfaces/ICardRepository.cs
+++ TelegramCards/Repositories/Interfaces/ICardRepository.cs
@@ -1,10 +1,11 @@
-﻿using TelegramCards.Models.Entitys;
+﻿using TelegramCards.Models.DTO;
+using TelegramCards.Models.Entitys;
 
 namespace TelegramCards.Repositories.Interfaces;
 
 public interface ICardRepository
 {
-    Task<Card> GenerateNewCardToUserAsync(long userTelegramId);
-    Task<(ICollection<Card> cards, int pageCount)> GetUserCardsAsync(long ownerId, int page, int pageSize);
-    Task<Card?> SendCardAsync(long senderId, long newOwnerId, long cardId);
+    Task<CardOutputDto?> GenerateNewCardToUserAsync(long userTelegramId);
+    Task<(ICollection<CardOutputDto> cards, int pageCount)> GetUserCardsAsync(long ownerId, int page, int pageSize);
+    Task<CardOutputDto?> SendCardAsync(long senderId, long newOwnerId, long cardId);
 }
