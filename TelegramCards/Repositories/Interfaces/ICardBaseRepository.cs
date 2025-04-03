35a2d4cd3e6bf2e05c00c8ac319ba568d1430feb
IvanSobolev <i@bezlikiy.ru>
2025-04-03 10:20:14 +0300

    fix: interface for busines logic

--- TelegramCards/Repositories/Interfaces/ICardBaseRepository.cs
+++ TelegramCards/Repositories/Interfaces/ICardBaseRepository.cs
@@ -5,11 +5,8 @@ namespace TelegramCards.Repositories.Interfaces;
 
 public interface ICardBaseRepository
 {
-    Task<CardBase> AddNewCardBaseAsync(long adminId, Rarity rarity, string photoUrl, int pointsNumber);
-    Task<ICollection<CardBase>> GetCardBasesAsync(long adminId, int page, int pageSize);
-    Task<CardBase> GetCardBasesAsync(Rarity rarityLevel, int cardIndex);
-    Task<int> GetCardsRarityCount(Rarity rarity);
-    Task<CardBase> UpdateCardBaseAsync(long adminId, long cardId, Rarity? rarity = null, string? photoUrl = null, int? pointsNumber = null);
-    Task DeleteCardBaseAsync(long adminId, long id);
+    Task<CardBase?> AddNewCardBaseAsync(long adminId, Rarity rarity, string photoUrl, int pointsNumber);
+    Task<(ICollection<CardBase> cardBases, int PageCount)> GetCardBasesAsync(long adminId, int page, int pageSize);
+    Task<int> GetLastIndexInRarity(Rarity rarity);
     
 }
