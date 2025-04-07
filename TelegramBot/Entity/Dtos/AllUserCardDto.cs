f837c2a6c1862fd9c320f0c8ab3b97b5ff719653
IvanSobolev <i@bezlikiy.ru>
2025-04-07 19:07:17 +0300

    feat: interfaces and dto for work with user and card

---
+++ TelegramBot/Entity/Dtos/AllUserCardDto.cs
@@ -0,0 +1,7 @@
+﻿namespace TelegramBot.Entity.Dtos;
+
+public class AllUserCardDto
+{
+    public ICollection<CardOutputDto> Cards { get; set; }
+    public int PageCount { get; set; }
+}
---
+++ TelegramBot/Entity/Dtos/CardOutputDto.cs
@@ -0,0 +1,14 @@
+﻿namespace TelegramBot.Entity.Dtos;
+
+public class CardOutputDto
+{
+    public long Id { get; set; }
+    public long OwnerId { get; set; }
+
+    public int CardBaseId { get; set; }
+    public string CardPhotoUrl { get; set; }
+    public int Points { get; set; }
+
+    public DateTime GenerationDate { get; set; }
+    public DateTime ReceivedCard { get; set; }
+}
