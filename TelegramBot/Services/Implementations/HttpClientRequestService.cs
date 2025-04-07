b3d6f6becb663cf9b81d6feb799f3c1b8b4aecbb
IvanSobolev <i@bezlikiy.ru>
2025-04-07 18:25:02 +0300

    feat: http client request service

---
+++ TelegramBot/Services/Implementations/HttpClientRequestService.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using System.Text.Json;
+using TelegramBot.Services.Interfaces;
+
+namespace TelegramBot.Services.Implementations;
+
+public class HttpClientRequestService(HttpClient? httpClient = null) : IRequestService
+{
+    private readonly HttpClient _httpClient = httpClient ?? new HttpClient();
+    
+    public async Task<T?> GetRequestAsync<T>(string url)
+    {
+        var response = await _httpClient.GetAsync(url);
+        response.EnsureSuccessStatusCode();
+        return await DeserializeResponse<T>(response);
+    }
+
+    public async Task<T?> PostRequestAsync<T, TB>(string url, TB body)
+    {
+        var content = SerializeRequest(body);
+        var response = await _httpClient.PostAsync(url, content);
+        response.EnsureSuccessStatusCode();
+        return await DeserializeResponse<T>(response);
+    }
+
+    public async Task<T?> PutRequestAsync<T, TB>(string url, TB body)
+    {
+        var content = SerializeRequest(body);
+        var response = await _httpClient.PutAsync(url, content);
+        response.EnsureSuccessStatusCode();
+        return await DeserializeResponse<T>(response);
+    }
+
+    public async Task<T?> DeleteRequestAsync<T>(string url)
+    {
+        var response = await _httpClient.DeleteAsync(url);
+        response.EnsureSuccessStatusCode();
+        return await DeserializeResponse<T>(response);
+    }
+    
+    private static StringContent SerializeRequest<T>(T body)
+    {
+        var json = JsonSerializer.Serialize(body);
+        return new StringContent(json, Encoding.UTF8, "application/json");
+    }
+
+    private static async Task<T?> DeserializeResponse<T>(HttpResponseMessage response)
+    {
+        var responseContent = await response.Content.ReadAsStringAsync();
+        return JsonSerializer.Deserialize<T>(responseContent, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        });
+    }
+}
---
+++ TelegramBot/Services/Interfaces/IRequestService.cs
@@ -0,0 +1,9 @@
+﻿namespace TelegramBot.Services.Interfaces;
+
+public interface IRequestService
+{
+    Task<T?> GetRequestAsync<T>(string url);
+    Task<T?> PostRequestAsync<T, TB>(string url, TB body);
+    Task<T?> PutRequestAsync<T, TB>(string url, TB body);
+    Task<T?> DeleteRequestAsync<T>(string url);
+}
