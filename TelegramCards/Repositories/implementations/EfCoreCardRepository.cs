58376590f05095b432fa74dc97c18852e87c1854
IvanSobolev <i@bezlikiy.ru>
2025-04-07 11:43:38 +0300

    fix: bug with you cant send card

--- TelegramCards/Repositories/implementations/EfCoreCardRepository.cs
+++ TelegramCards/Repositories/implementations/EfCoreCardRepository.cs
@@ -71,16 +71,8 @@ public class EfCoreCardRepository(DataContext dataContext, ICardBaseGeneratorSer
     /// <inheritdoc/>
     public async Task<CardOutputDto?> SendCardAsync(long senderId, long newOwnerId, long cardId)
     {
-        var card = await _dataContext.Cards.Select(c => new CardOutputDto
-            {
-                Id = c.Id,
-                OwnerId = c.OwnerId,
-                CardBaseId = c.CardBaseId,
-                CardPhotoUrl = c.BaseCard.CardPhotoUrl,
-                Points = c.BaseCard.Points,
-                GenerationDate = c.GenerationDate,
-                ReceivedCard = c.ReceivedCard
-            })
+        var card = await _dataContext.Cards
+            .Include(c => c.BaseCard)
             .FirstOrDefaultAsync(c => c.Id == cardId);
         if (card == null)
         {
@@ -88,11 +80,30 @@ public class EfCoreCardRepository(DataContext dataContext, ICardBaseGeneratorSer
         }
         if (card.OwnerId != senderId)
         {
-            return card;
+            return new CardOutputDto
+            {
+                Id = card.Id,
+                OwnerId = card.OwnerId,
+                CardBaseId = card.CardBaseId,
+                CardPhotoUrl = card.BaseCard.CardPhotoUrl,
+                Points = card.BaseCard.Points,
+                GenerationDate = card.GenerationDate,
+                ReceivedCard = card.ReceivedCard
+            };
         }
 
         card.OwnerId = newOwnerId;
+        card.ReceivedCard = DateTime.UtcNow;
         await _dataContext.SaveChangesAsync();
-        return card;
+        return new CardOutputDto
+        {
+            Id = card.Id,
+            OwnerId = card.OwnerId,
+            CardBaseId = card.CardBaseId,
+            CardPhotoUrl = card.BaseCard.CardPhotoUrl,
+            Points = card.BaseCard.Points,
+            GenerationDate = card.GenerationDate,
+            ReceivedCard = card.ReceivedCard
+        };
     }
 }
